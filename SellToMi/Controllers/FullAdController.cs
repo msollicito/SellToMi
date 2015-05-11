6ff65ba02695c82b28f695c146db0653292055ce
msollicito <michelle.sollicito@gmail.com>
2015-05-11 08:32:25 -0400

    New version of SellToMi
    This is TxtToAd recreated as MVC4

---
+++ SellToMi/Controllers/FullAdController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SellToMi.Models;
+
+namespace SellToMi.Controllers
+{
+    public class DisplayAdViewModel
+    {
+        public GetFullAd_Result AdObject { get; set; }
+        public List<GetSubCategories_Result> SubCatObject { get; set; }
+    }  
+    public class FullAdController : Controller
+    {
+        private DB_77381_txttoadEntities db = new DB_77381_txttoadEntities();
+
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View(db.vwFullAdItem.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+        
+        public ActionResult DisplayAd(int id = 0)
+        {
+            List<GetFullAd_Result> vwfulladitem = db.GetFullAd(id).ToList<GetFullAd_Result>();
+            if (vwfulladitem.Count() == 0)
+            {
+                return HttpNotFound();
+            }
+            GetFullAd_Result fullAd = vwfulladitem.ElementAt(0);
+
+            List<GetSubCategories_Result> SubCats = db.GetSubCategories(fullAd.CategoryId).ToList<GetSubCategories_Result>();
+
+            var DisplayAdViewModel = new DisplayAdViewModel
+            {
+                AdObject = fullAd,
+                SubCatObject = SubCats
+            };
+            return View(DisplayAdViewModel);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        public ActionResult Create(vwFullAdItem vwfulladitem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.vwFullAdItem.Add(vwfulladitem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vwfulladitem);
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            vwFullAdItem vwfulladitem = db.vwFullAdItem.Find(id);
+            if (vwfulladitem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vwfulladitem);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(vwFullAdItem vwfulladitem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vwfulladitem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vwfulladitem);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            vwFullAdItem vwfulladitem = db.vwFullAdItem.Find(id);
+            if (vwfulladitem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vwfulladitem);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            vwFullAdItem vwfulladitem = db.vwFullAdItem.Find(id);
+            db.vwFullAdItem.Remove(vwfulladitem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SellToMi/Controllers/SubCategoriesController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SellToMi.Models;
+
+namespace SellToMi.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        //
+        // GET: /SubCategories/
+        private DB_77381_txttoadEntities db = new DB_77381_txttoadEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /SubCategories/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /SubCategories/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /SubCategories/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /SubCategories/Edit/5
+
+       
+        public ActionResult GetSubcategories(int id)
+        {
+            IEnumerable<SellToMi.Models.GetSubCategories_Result> subcats = db.GetSubCategories(id);
+            if (subcats.Count() == 0)
+            {
+                return HttpNotFound();
+            }
+            return View(subcats);
+        }
+        //
+        // POST: /SubCategories/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /SubCategories/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /SubCategories/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
